cf4629250f3db00467bc3da301754aaffe21661c
Radwa-Hafez <139390139+Radwa-Hafez@users.noreply.github.com>
2023-10-05 21:11:21 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project was coded by Radwa Hafez and is{" "}
+        <a
+          href="https://github.com/Radwa-Hafez/final-weather-react"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
