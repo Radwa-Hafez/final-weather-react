423bf266cf7793a6c8c91510559e5a4541c37fb4
Radwa-Hafez <139390139+Radwa-Hafez@users.noreply.github.com>
2023-10-05 21:47:27 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project was coded by Radwa Hafez and is{" "}
-        <a
-          href="https://github.com/Radwa-Hafez/final-weather-react"
-          target="_blank"
-          rel="noopener"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project was coded by Radwa Hafez and is{" "}
+          <a
+            href="https://github.com/Radwa-Hafez/final-weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
