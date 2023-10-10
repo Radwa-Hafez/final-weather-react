f5f17a05df21d4d20cc5f5691deda3067133b906
Radwa-Hafez <139390139+Radwa-Hafez@users.noreply.github.com>
2023-10-10 17:24:41 +0200

    Refactored some code

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,6 +16,13 @@ export default function WeatherForecast(props) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
+  function load() {
+    const apiKey = "8cd9be374c7c96c39a9fe73f4bf2f055";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleRespose);
+  }
 
   if (loaded) {
     return (
@@ -28,17 +35,15 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
       </div>
     );
   } else {
-    const apiKey = "8cd9be374c7c96c39a9fe73f4bf2f055";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleRespose);
+    load();
     return null;
   }
 }
